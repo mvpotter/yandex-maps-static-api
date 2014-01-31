ccaac950a81e2461e8c1b592eebe38aa999cb789
mvpotter <supermegapotter@gmail.com>
2014-01-31 16:02:02 +0700

    Checkstyle errors are fixed

--- yandex-maps-api-core/src/main/java/constants/MapType.java
+++ yandex-maps-api-core/src/main/java/constants/MapType.java
@@ -6,42 +6,42 @@ package main.java.constants;
  * Date: 31.01.14
  * Time: 15:45
  *
- * Defines a map's type (basic, satellite, hybrid and so on)
+ * Defines a map's type (basic, satellite, hybrid and so on).
  */
 public enum MapType {
 
     /**
-     * Basic map
+     * Basic map.
      */
     MAP("map"),
     /**
-     * National map (with corrections added by Yandex Maps users)
+     * National map (with corrections added by Yandex Maps users).
      */
     NATIONAL_MAP("pmap"),
     /**
-     * Satelite view
+     * Satelite view.
      */
     SATELLITE("sat"),
     /**
-     * Includes names of geographical objects
+     * Includes names of geographical objects.
      */
     GEO_NAMES("skl"),
     /**
-     * Includes names of geographical objects (with corrections added by Yandex Maps users)
+     * Includes names of geographical objects (with corrections added by Yandex Maps users).
      */
     NATIONAL_GEO_NAMES("pskl"),
     /**
-     * Hybrid view: satellite view with the names of geographical objects
+     * Hybrid view: satellite view with the names of geographical objects.
      */
     HYBRID("sat,skl"),
     /**
-     * Traffic jams layer
+     * Traffic jams layer.
      */
     TRAFFIC_JAMS("trf");
 
     private String code;
 
-    private MapType(String code) {
+    private MapType(final String code) {
         this.code = code;
     }
 
