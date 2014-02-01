c1b1ab27ad09b50b9f846a2165167ba3632876f4
mvpotter <michaelpotter@mail.ru>
2014-02-01 13:50:50 +0700

    Added support for map apiKey, viewport, scale and size

--- yandex-maps-static-api/src/test/java/io/github/mvpotter/YandexApiUrlBuilderTest.java
+++ yandex-maps-static-api/src/test/java/io/github/mvpotter/YandexApiUrlBuilderTest.java
@@ -6,18 +6,64 @@
  */
 package io.github.mvpotter;
 
+import io.github.mvpotter.model.Coordinate;
+import io.github.mvpotter.model.Size;
 import org.junit.Assert;
 import org.junit.Test;
 import io.github.mvpotter.model.YandexMap;
 
 public class YandexApiUrlBuilderTest {
-
+    // Basic URL
     private static final String DEFAULT_API_URL = "http://static-maps.yandex.ru/1.x/?l=map&ll=83.098206,54.851704";
+    // API key
+    private static final String API_KEY = "5kgRTG54gYB43g5HR4v2";
+    private static final String API_URL_WITH_API_KEY = DEFAULT_API_URL + "&apiKey=5kgRTG54gYB43g5HR4v2";
+    // Viewport
+    private static final float VIEWPORT_LONGITUDE = 0.05f;
+    private static final float VIEWPORT_LATITUDE = 0.05f;
+    private static final String API_URL_WITH_VIEWPORT = DEFAULT_API_URL + "&spn=" + VIEWPORT_LONGITUDE + "," +
+                                                                                    VIEWPORT_LATITUDE;
+    // Scale
+    private static final int SCALE = 1;
+    private static final String API_URL_WITH_SCALE = DEFAULT_API_URL + "&z=" + SCALE;
+    // Size
+    private static final int WIDTH = 250;
+    private static final int HEIGHT = 200;
+    private static final String API_URL_WITH_SIZE = DEFAULT_API_URL + "&size=" + WIDTH + "," + HEIGHT;
 
     @Test
     public void basicTestBuild() {
         YandexMap yandexMap = new YandexMap();
         Assert.assertEquals(DEFAULT_API_URL, YandexApiUrlBuilder.build(yandexMap));
+    }
+
+    @Test
+    public void testBuildWithApiKey() {
+        YandexMap yandexMap = new YandexMap();
+        yandexMap.setApiKey(API_KEY);
+        System.out.println(YandexApiUrlBuilder.build(yandexMap));
+        Assert.assertEquals(API_URL_WITH_API_KEY, YandexApiUrlBuilder.build(yandexMap));
+    }
+
+    @Test
+    public void testBuildWithViewport() {
+        YandexMap yandexMap = new YandexMap();
+        yandexMap.setViewport(new Coordinate(VIEWPORT_LONGITUDE, VIEWPORT_LATITUDE));
+        Assert.assertEquals(API_URL_WITH_VIEWPORT, YandexApiUrlBuilder.build(yandexMap));
+    }
+
+    @Test
+    public void testBuildWithScale() {
+        YandexMap yandexMap = new YandexMap();
+        yandexMap.setScale(SCALE);
+        Assert.assertEquals(API_URL_WITH_SCALE, YandexApiUrlBuilder.build(yandexMap));
+    }
+
+    @Test
+    public void testBuildWithSize() {
+        YandexMap yandexMap = new YandexMap();
+        yandexMap.setSize(new Size(WIDTH, HEIGHT));
+        Assert.assertEquals(API_URL_WITH_SIZE, YandexApiUrlBuilder.build(yandexMap));
     }
 
 }
